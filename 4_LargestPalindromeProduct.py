a7f90c11194a485c044398bd57db96888a8b4594
MShahmeer <shahmeer.athar@gmail>
2016-07-21 23:17:15 +0500

    Completed exercise 4
    While the answer is correct, the logic isn’t entirely sound as the same
    calculation is being repeated multiple times within the for in loop.
    I’ll fix this soon.

---
+++ 4_LargestPalindromeProduct.py
@@ -0,0 +1,21 @@
+threeDigitNumbers1 = []
+threeDigitNumbers2 = []
+palindromes = []
+
+def reverse_int(i):
+    return int(str(i)[::-1])
+
+for number in xrange(100, 1000):
+    threeDigitNumbers1.append(number)
+
+threeDigitNumbers2 = threeDigitNumbers1
+
+for number in threeDigitNumbers1:
+    for num in threeDigitNumbers2:
+        potentialPalindrome = number * num
+        reverseOfPotentialPalindrome = reverse_int(potentialPalindrome)
+        if potentialPalindrome == reverseOfPotentialPalindrome:
+            palindromes.append(potentialPalindrome)
+
+largestPalindrome = max(palindromes)
+print(largestPalindrome)
